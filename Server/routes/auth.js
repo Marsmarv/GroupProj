2f56d681e613bf55350659da480ea5b92af3bcfa
Marvin Romero <marvin.r02@gmail.com>
2019-06-14 20:54:20 -0600

    last push

---
+++ Server/routes/auth.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const User = require('../models/user')
+const authRouter = express.Router()
+const jwt = require('jsonwebtoken')
+
+authRouter.post('/signup', (res, res, next) => {
+  User.findOne({username: req.body.username}, (err, existingUser) => {
+    if (err) {
+      res.status(500)
+      return next(err)
+    }
+
+    if (existingUser !== null) {
+      res.status(400)
+      return next(new Error('Username already exists'))
+    }
+
+    const newUser = new User(req.body)
+    newUser.save((err, user) => {
+      if (err) {
+        res.status(500)
+        return next(err)
+      }
+
+      const token = jwt.sign(user.toObject(), process.env.SECRET)
+      return res.status(201).send({success: true, user: user.toObject(), token})
+    })
+  })
+})
+
