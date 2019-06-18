2938ec0500dd254066fca9e174157b454f353806
Tom Eschler <tomeschlercoding@gmail.com>
2019-06-18 17:52:41 -0600

    final draft

--- Client/src/components/GlobalProvider.js
+++ Client/src/components/GlobalProvider.js
@@ -1,49 +1,34 @@
 import React from 'react'
-import Axios from 'axios'
 const {Consumer,Provider} = React.createContext()
 
-class About extends React.Component{
+class GlobalProvider extends React.Component{
     constructor() {
-    super()
-    this.state = {
-        positionY: 200,
+        super()
+        this.state = {
+            positionY: 200,
+        }
     }
-    }
-    
     handlePositionY = (event) => {
-    console.log(event.which)
+        console.log(event.which)
         if(event.which === 40){
             this.setState(prev => {
                 return {
                     positionY: prev.positionY +10
-                }
-            })
+            }
+        })
         } else if(event.which === 38) {
             this.setState(prev => {
                 return {
-                    positionY: prev.positionY -10
-            }
-        })
-    }
-    }
-    componentDidMount() {
-        window.addEventListener('keydown', this.handlePositionY)
-    }
-    componentWillMount(){
-        window.removeEventListener('keydown', () => console.log('hello'))
+                positionY: prev.positionY -10
+                }
+            })
+        }
     }
     render(){
         return(
             <Provider value={{
                 ...this.state,
-                getDailyPhoto: this.getDailyPhoto ,
-                noDisplayPhoto: this.noDisplayPhoto,
-                getPlanetPhotos: this.getPlanetPhotos,
-                displayPlanets: this.displayPlanets,
-                openMenu: this.openMenu,
-                closeMenu: this.closeMenu,
-                getGalaxyPhotos: this.getGalaxyPhotos,
-                displayGalaxies: this.displayGalaxies
+                handlePositionY: this.handlePositionY
             }}>{this.props.children}
             </Provider>
         )
