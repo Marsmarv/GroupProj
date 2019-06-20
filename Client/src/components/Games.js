13616e46452584026498b2f87aa02cbb26842531
Marvin Romero <marvin.r02@gmail.com>
2019-06-20 13:21:06 -0600

    fades

--- Client/src/components/Games.js
+++ Client/src/components/Games.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Fade from 'react-reveal/Fade'
-// import TicTacToeApp from '../tic-tac-toe/TicTacToeApp'
 import ConnectFourApp from '../connect-four/ConnectFourApp'
 import { Link } from "react-router-dom";
 
@@ -10,12 +9,10 @@ const Games = () => {
   return (
     <Fade left>
       <div className='games content'>
-        {/* <Link to='/games/tic-tac-toe'>TicTacToe</Link> */}
         <Fade right><Link to='/games/connect-four'>ConnectFour</Link></Fade>
         <div className='games-content'>
           <Switch>
             <Route path='/games/connect-four' component={ConnectFourApp} />
-            {/* <Route path='/games/tic-tac-toe' component={TicTacToeApp} /> */}
           </Switch>
         </div>
       </div>
