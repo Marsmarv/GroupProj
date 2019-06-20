9703477835557c0396ebcd6c59421f14d9aac302
Marvin Romero <marvin.r02@gmail.com>
2019-06-20 09:31:30 -0600

    server stuff

--- Client/src/components/Login.js
+++ Client/src/components/Login.js
@@ -3,20 +3,27 @@ import {withGlobalProvider} from './GlobalProvider'
 import Fade from 'react-reveal/Fade'
 
 const Login = (props) => {
-  const { handleChange, username, password, userSignUp } = props
+  const { handleChange, username, password, userSignUp, userLogin } = props
 
-  const handleSubmit = (e) => {
-      e.preventDefault()
-      userSignUp({username, password}).then(()=> {
-        props.history.push('/')
-      })
-      alert(`successful sign up for ${props.username}`)
+  const handleLogin = (e) => {
+    e.preventDefault()
+    userLogin({username, password}).then(()=> {
+      props.history.push('/games')
+    })
+    alert(`successfully logged in for ${props.username}`)
   }
+  const handleSignup = (e) => {
+    e.preventDefault()
+    userSignUp({username, password}).then(()=> {
+      props.history.push('/')
+    })
+    alert(`successful sign up for ${props.username}`)
+}
 
   return (
     <Fade right>
       <div className="login-container">
-      <form className="form-set" onSubmit={handleSubmit}>
+      <form className="form-set">
         <input onChange={handleChange} 
         type="text" 
         name='username'
@@ -27,8 +34,8 @@ const Login = (props) => {
         name='password'
         placeholder="Password"
         value={password} /> <br/><br/>
-        <button>Sign up</button>
-        <button>Log in</button>
+        <button onClick={handleSignup}>Sign up</button>
+        <button onClick={handleLogin}>Log in</button>
       </form>
       </div>
         <h3 className="welcome">Welcome {props.username}!</h3>
