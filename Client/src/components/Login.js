7935442bb46453623cbba6240d102e0093d880ad
Marvin Romero <marvin.r02@gmail.com>
2019-06-19 18:31:28 -0600

    login done also some css

--- Client/src/components/Login.js
+++ Client/src/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {withGlobalProvider} from './GlobalProvider'
+import Fade from 'react-reveal/Fade'
 
 const Login = (props) => {
   const { handleChange, username, password, userSignUp } = props
@@ -13,22 +14,23 @@ const Login = (props) => {
   }
 
   return (
-    <div className="login-container">
-
-    <form className="form-set" onSubmit={handleSubmit}>
-      <input onChange={handleChange} 
-      type="text" 
-      name='username'
-      placeholder="Username"
-      value={username} /> <br/><br/>
-      <input onChange={handleChange} 
-      type="text" 
-      name='password'
-      placeholder="Password"
-      value={password} /> <br/>
-      <button>Log in</button>
-    </form>
-    </div>
+    <Fade right>
+      <div className="login-container">
+      <form className="form-set" onSubmit={handleSubmit}>
+        <input onChange={handleChange} 
+        type="text" 
+        name='username'
+        placeholder="Username"
+        value={username} /> <br/><br/>
+        <input onChange={handleChange} 
+        type="text" 
+        name='password'
+        placeholder="Password"
+        value={password} /> <br/>
+        <button>Log in</button>
+      </form>
+      </div>
+    </Fade>
   )
 }
 
