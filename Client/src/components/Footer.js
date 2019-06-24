3c00c3e41edbd72f98a11ecddef82405a66087a5
chans7692 <chans7692@gmail.com>
2019-06-24 08:53:44 -0600

    Finall Product

--- Client/src/components/Footer.js
+++ Client/src/components/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Joystick } from 'react-joystick-component';
 import { Link } from "react-router-dom";
 import {withGlobalProvider} from './GlobalProvider'
